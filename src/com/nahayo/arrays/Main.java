04ffaa4825583ac606ec57d9d0f259c2601652df
Alex Nahayo <alexn794@gmail.com>
2023-04-13 21:01:39 +0100

    added reverse method in LL Class

--- src/com/nahayo/arrays/Main.java
+++ src/com/nahayo/arrays/Main.java
@@ -2,12 +2,11 @@ package com.nahayo.arrays;
 
 import com.nahayo.linkedlists.LinkedList;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
+
 
     }
 
--- src/com/nahayo/linkedlists/LinkedList.java
+++ src/com/nahayo/linkedlists/LinkedList.java
@@ -5,88 +5,86 @@ import java.util.NoSuchElementException;
 
 public class LinkedList {
     private int nodeCount;
+
     private class Node {
         private int value;
         private Node next;
+
         public Node(int value) {
             this.value = value;
         }
     }
+
     private Node first;
     private Node last;
 
-    public void addLast(int item){
+    public void addLast(int item) {
         nodeCount++;
         Node node = new Node(item);
-        if(isEmpty()){
-            first = last =  node;
-        }
-        else {
+        if (isEmpty()) {
+            first = last = node;
+        } else {
             last.next = node;
             last = node;
         }
     }
 
-    public void addFirst(int item){
+    public void addFirst(int item) {
         nodeCount++;
         Node node = new Node(item);
-        if(isEmpty()){
-            first = last =  node;
-        }
-        else {
+        if (isEmpty()) {
+            first = last = node;
+        } else {
             node.next = first;
             first = node;
         }
     }
-    public int indexOf(int value){
+
+    public int indexOf(int value) {
         Node pointer;
-        int count = 0 ;
+        int count = 0;
         pointer = first;
-        while (pointer != null){
-            if (value == pointer.value){
+        while (pointer != null) {
+            if (value == pointer.value) {
                 return count;
-            }
-            else {
-                 pointer = pointer.next;
-                 count++;
+            } else {
+                pointer = pointer.next;
+                count++;
             }
         }
         return -1;
     }
 
-    public boolean contains(int item){
-        if(indexOf(item) !=-1 ){
+    public boolean contains(int item) {
+        if (indexOf(item) != -1) {
             return true;
         } else {
             return false;
         }
     }
-    public void removeFirst(){
-        if (isEmpty()){
+
+    public void removeFirst() {
+        if (isEmpty()) {
             throw new NoSuchElementException();
-        }
-        else if (nodeCount==1) {
+        } else if (nodeCount == 1) {
             first = last = null;
-        }
-        else {
-             Node pointer = first;
-             pointer = first.next;
-             first.next = null;
-             first = pointer;
+        } else {
+            Node pointer = first;
+            pointer = first.next;
+            first.next = null;
+            first = pointer;
         }
         nodeCount--;
     }
 
-    public void removeLast(){
-        if (isEmpty()){
+    public void removeLast() {
+        if (isEmpty()) {
             throw new NoSuchElementException();
-        }
-        else if(nodeCount==1){
+        } else if (nodeCount == 1) {
             first = last = null;
-        }
-        else {
+        } else {
             Node pointer = first;
-            while(pointer.next.next != null){
+            while (pointer.next.next != null) {
                 pointer = pointer.next;
             }
             last = pointer;
@@ -95,18 +93,19 @@ public class LinkedList {
         nodeCount--;
     }
 
-    public int size(){
+    public int size() {
         return nodeCount;
     }
 
-    private boolean isEmpty(){
-        if (first==null){
+    private boolean isEmpty() {
+        if (first == null) {
             return true;
         } else {
             return false;
         }
     }
-    public int[] toArray(){
+
+    public int[] toArray() {
         int array[] = new int[nodeCount];
         Node pointer = first;
         for (int i = 0; i < array.length; i++) {
@@ -114,5 +113,25 @@ public class LinkedList {
             pointer = pointer.next;
         }
         return array;
+    }
+
+    public void reverse() {
+        if (isEmpty()) {
+            return;
+        }
+        else {
+            Node previous = first;
+            Node current = previous.next;
+            System.out.println(current);
+            while (current!=null) {
+                Node next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+            last = first;
+            last.next = null;
+            first = previous;
+        }
     }
 }
