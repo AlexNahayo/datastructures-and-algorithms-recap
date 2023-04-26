32a3a1fb2dd9a2ef00872ea43afef4c410aa2e6d
Alex Nahayo <alexn794@gmail.com>
2023-04-26 20:42:41 +0100

    added ReverseString Class to Stacks dir

--- src/com/nahayo/stacks/ReverseString.java
+++ src/com/nahayo/stacks/ReverseString.java
@@ -1,5 +1,21 @@
 package com.nahayo.stacks;
 
+import java.util.Stack;
+
 public class ReverseString {
-// to do
+    public String reverse(String str) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder reversed = new StringBuilder();
+        if (str == null) {
+        throw new IllegalArgumentException();
+        } else {
+            for (char c : str.toCharArray()) {
+                stack.push(c);
+            }
+            while (!stack.isEmpty()) {
+                reversed.append(stack.pop());
+            }
+        }
+        return reversed.toString();
+    }
 }
