108cfc8f935d71dd08936475c2dd627154131eb3
Alex Nahayo <alexn794@gmail.com>
2023-04-27 20:32:20 +0100

    added pop method to StacksByArray Class

--- src/com/nahayo/stacks/StacksByArray.java
+++ src/com/nahayo/stacks/StacksByArray.java
@@ -1,5 +1,7 @@
 package com.nahayo.stacks;
 
+import java.util.EmptyStackException;
+
 public class StacksByArray<T> {
     private int count = 0;
     int[] stack;
@@ -16,7 +18,23 @@ public class StacksByArray<T> {
         stack = biggerStack;
         stack[count - 1] = item;
     }
-
+    //pop
+    public int pop(){
+        if (count <= 0){
+            throw new EmptyStackException();
+        }
+        else {
+            count--;
+            int poppedItem;
+            int[] smallerStack = new int[count];
+            for (int i=0; i<smallerStack.length;i++){
+                smallerStack[i] =  stack[i];
+            }
+            poppedItem = stack[stack.length -1];
+            stack = smallerStack;
+            return poppedItem;
+        }
+    }
     public void printStack(){
         for (int i =0; i<stack.length;i++){
             System.out.println(stack[i]);
