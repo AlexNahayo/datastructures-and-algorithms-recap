bb43c1ea83ad637231092f2b1cadd468fd669589
Alex Nahayo <alexn794@gmail.com>
2023-05-02 19:58:55 +0100

    added TwoStacks Class - **STILL PENDING**

---
+++ src/com/nahayo/stacks/TwoStacks.java
@@ -0,0 +1,24 @@
+package com.nahayo.stacks;
+
+
+/**
+ * Question:
+ * Implement two stacks in one array. Support these operations:
+ * push1() // to push in the first
+ * stackpush2() // to push in the second stack
+ * pop1()
+ * pop2()
+ * isEmpty1()
+ * isEmpty2()
+ * isFull1()
+ * isFull2()
+ *
+ * Make sure your implementation is space efficient.
+ * (hint: do not allocate the same amount of space by dividing the array in half.)
+ *
+ * Below is a Class that tries to solve the above
+ *
+ * @author  Alex Nahayo
+ */
+public class TwoStacks {
+}
