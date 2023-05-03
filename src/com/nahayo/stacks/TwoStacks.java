88050378691bff16d18c09bc81d5c01accacc94e
Alex Nahayo <alexn794@gmail.com>
2023-05-03 21:10:19 +0100

    added push1/2, isEmpty1/2 & isFull1/2 methods

--- src/com/nahayo/stacks/TwoStacks.java
+++ src/com/nahayo/stacks/TwoStacks.java
@@ -16,9 +16,58 @@ package com.nahayo.stacks;
  * Make sure your implementation is space efficient.
  * (hint: do not allocate the same amount of space by dividing the array in half.)
  *
- * Below is a Class that tries to solve the above
+ * Below is a Class that tries to solve the above.
  *
  * @author  Alex Nahayo
  */
 public class TwoStacks {
+
+    int[] twoStackSize;
+    int stack1Count = 0;
+    int stack2Count = 0;
+
+    public TwoStacks(int size) {
+        if(!(size % 2 == 0)){
+            throw new NumberFormatException();
+        }
+        else {
+            twoStackSize = new int[size];
+        }
+    }
+
+    public void push1(int item){
+        if(stack1Count < twoStackSize.length/2)
+            twoStackSize[stack1Count] = item;
+            stack1Count++;
+        }
+
+    public void push2(int item){
+        if(stack2Count < twoStackSize.length/2) {
+            twoStackSize[twoStackSize.length / 2 + stack2Count] = item;
+            stack2Count++;
+        }
+    }
+
+    public boolean isEmpty1() {
+        return stack1Count == 0;
+    }
+    public boolean isEmpty2(){
+        return stack2Count ==0;
+    }
+
+    public boolean isFull1(){
+        return stack1Count == twoStackSize.length/2;
+    }
+
+    public boolean isFull2(){
+        return stack2Count == twoStackSize.length/2;
+    }
+
+    public void printStack(){
+        for (int item : twoStackSize){
+            System.out.println(item);
+        }
+    }
+
+    //Todo - pop1 and pop2 methods as well as some more tests. 
 }
