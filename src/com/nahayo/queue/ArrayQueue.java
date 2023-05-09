1eb5541cb8a5571b87c2a7941b85eefe9be2f6a8
Alex Nahayo <alexn794@gmail.com>
2023-05-09 20:31:23 +0100

    added the following methods enqueue, dequeue, peek, isEmpty, isFull and printArrayQueue methods

--- src/com/nahayo/queue/ArrayQueue.java
+++ src/com/nahayo/queue/ArrayQueue.java
@@ -1,10 +1,49 @@
 package com.nahayo.queue;
 
 public class ArrayQueue {
-    //Todo
-    //enqueue
-    //enqueue
-    //peek
-    //isEmpty
-    //isFull
+
+    private final int[] array;
+    private int front, back, count = 0;
+
+    public ArrayQueue(int size) {
+        array = new int[size];
+    }
+
+    public void enqueue(int item){
+        if(count > array.length){
+            throw new IndexOutOfBoundsException();
+        }
+        else{
+            array[count] = item;
+            back = count;
+        }
+        count++;
+    }
+
+    public void dequeue(){
+        if(isEmpty()){
+            // Need to find out what exception can be thrown here!
+        }
+        else {
+            ++front;
+        }
+    }
+
+    public int peek(){
+        return array[front];
+    }
+
+    public boolean isEmpty(){
+        return front ==  back;
+    }
+
+    public boolean isFull(){
+        return back == array.length - 1;
+    }
+
+    public void printArrayQueue(){
+        for (int i = front; i <= back ; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
