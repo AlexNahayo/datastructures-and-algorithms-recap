da36e95d3b392b6a39298085bbcd14e613370a87
Alex Nahayo <alexn794@gmail.com>
2023-06-14 20:35:18 +0100

    added HashTablExercises Class

--- src/com/nahayo/hashmap/HashTableExercises.java
+++ src/com/nahayo/hashmap/HashTableExercises.java
@@ -1,2 +1,24 @@
-package com.nahayo.hashmap;public class HashTableExercises {
+package com.nahayo.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashTableExercises {
+
+    //STILL TO DO....
+    public int mostFrequent(int [] elements){
+        Map<Integer, Integer> mostFrequentTall = new HashMap();
+        int unique = 0 ;
+        //find unique element as KEY and store occurrence as the value.
+        //nested for  1. loop will obtain key 2. loop will obtain a value of the number of occurrences.
+        //still to figure out how we are going to get the value.
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+            }
+            if(elements[i] == i){
+               // count
+            }
+        }
+        return 0;
+    }
 }
