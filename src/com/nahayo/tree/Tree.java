7fa2be86249402db62b873a18fa0d822519c4c3b
Alex Nahayo <alexn794@gmail.com>
2023-07-12 20:33:14 +0100

    added insert method to tree class

--- src/com/nahayo/tree/Tree.java
+++ src/com/nahayo/tree/Tree.java
@@ -2,61 +2,43 @@ package com.nahayo.tree;
 
 
 public class Tree {
-
-    Node root;
+    private Node root;
     private class Node {
         private int value;
-
-        Node leftChild;
-
-        Node rightChild;
+        private Node leftChild;
+        private Node rightChild;
 
         public Node(int value) {
             this.value = value;
         }
+        @Override
+        public String toString() {
+            return "Node=" + value;
+        }
     }
 
-    public Tree() {
-        root = new Node(0);
-    }
-
-    //Todo
     public void insert(int value){
-        Node pointer;
-        if(root.value == 0) {
-            root.value = value;
+        Node newNode = new Node(value);
+        if(root == null) {
+            root = newNode;
         }
-        else {
-            Node newNode = new Node(value);
-            // pointer needs to start at root
-            // check value of current node you are pointing to
-            // 1. if value > than current node value then
-            //  a) if the current node.rightChild == null then point, right child to current pointer node -- point new node to current pointer, then current pointer to new node.
-            //  b) if the current node.rightChild != null then keep drilling down until you get to
-            // 2. if value < than current node value then
-            //  a) if the current node.leftChild == null then point, left child to current pointer node -- point new node to current pointer, then current pointer to new node.
-            //  b) if the current node.leftChild != null then keep drilling down until you get to
-            pointer = this.root;
-
-            while (pointer.leftChild != null || pointer.rightChild!= null  ){
-                if(newNode.value > pointer.value){
-                    pointer = pointer.rightChild;
+        Node current = root;
+        while (true){
+            if(value < current.value){
+                if(current.leftChild == null){
+                    current.leftChild = newNode;
+                    break;
                 }
-                else if (newNode.value > pointer.value){
-                    pointer = pointer.leftChild;
+                current = current.leftChild;
+            }
+            else {
+                if(current.rightChild == null){
+                    current.rightChild = newNode;
+                    break;
                 }
+                current = current.rightChild;
             }
-
-            //This is last portion to actually add to the data
-            if(newNode.value > pointer.value && pointer.rightChild == null ) {
-                pointer.rightChild = newNode;
-            }
-            else if (newNode.value < pointer.value && pointer.leftChild == null) {
-                pointer.leftChild = newNode;
-            }
-
         }
-
     }
 
     //Todo
