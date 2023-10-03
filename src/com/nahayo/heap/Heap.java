31a0a829bbafddd5f4debaefa972da07dc4b035e
Alex Nahayo <alexn794@gmail.com>
2023-10-03 20:01:04 +0100

    added skeleton heap class

---
+++ src/com/nahayo/heap/Heap.java
@@ -0,0 +1,16 @@
+package com.nahayo.heap;
+
+public class Heap {
+    int [] elements;
+
+    public Heap(int size) {
+       elements = new int[size];
+    }
+
+    public void insert(int element){
+        //when heap is empty, then just add element to the first position in the array
+
+        //given a fixed sized heap
+        //1)
+    }
+}
