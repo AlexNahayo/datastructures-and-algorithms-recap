252f7c982b9e206b87c3814e4804a0068fc06331
Alex Nahayo <alexn794@gmail.com>
2026-03-25 19:51:02 +0000

    leetcode joys.

--- src/com/nahayo/leetcode/string/IsAnagram.java
+++ src/com/nahayo/leetcode/string/IsAnagram.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class IsAnagram {
 
-    /**Time O(n) & Space O(n)**/
+    /**Time O(s + t) & Space O(s + t)**/
     public boolean bruteForceSol(String s, String t) {
 
         // If lengths differ, they cannot be anagrams
@@ -42,6 +42,31 @@ public class IsAnagram {
         }
 
         return true;
+    }
+
+    /**Time O(s + t) & Space O(1)**/
+    public boolean optimalSol(String s, String t) {
+        // If lengths differ, they cannot be anagrams
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26]; // 26 letters in the English alphabet
+
+        // Count characters in s
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;  // 1) determines pos in the alphabet & 2) increments count for this char.
+        }
+
+        // Subtract counts using t
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;  // decrement count for this char
+            if (count[t.charAt(i) - 'a'] < 0) { //check if current value which exists is less than 0.
+                return false; // more of this char in t than in s
+            }
+        }
+
+        return true; // all counts balanced (original count array values should be "0")
     }
 }
 
